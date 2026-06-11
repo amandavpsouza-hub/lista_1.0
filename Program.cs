fdb8a05b72edd587274e023c4c70b6a90e9d1407
Amanda <amanda_vp_souza@estudante.sesisenai.org.br>
2026-06-11 20:08:04 -0300

    q1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿ // Solicita a idade do visitante
+        Console.WriteLine("Informe a idade do visitante:");
+
+        // Lê a idade digitada
+        int idade = int.Parse(Console.ReadLine());
+
+        // Verifica a idade
+        if (idade >= 16)
+        {
+            Console.WriteLine("Entrada liberada.");
+        }
+        else
+        {
+            Console.WriteLine("Entrada permitida somente com responsável.");
+        }
+    
+    
