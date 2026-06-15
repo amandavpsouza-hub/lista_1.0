857a8cc1a3ef930cc4672c5082fc20d7e0e07d82
Amanda <amanda_vp_souza@estudante.sesisenai.org.br>
2026-06-15 19:53:26 -0300

    q18

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
-﻿
-        int nota;
+﻿    { int quantidadeClientes;
+        int tempoAtendimento;
+        int tempoTotal = 0;
+        int tempoMedio;
 
-        Console.WriteLine("Informe uma nota de 0 a 10:");
-        nota = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Informe a quantidade de clientes atendidos:");
+        quantidadeClientes = Convert.ToInt32(Console.ReadLine());
 
-        while (nota < 0 || nota > 10)
+        for (int i = 1; i <= quantidadeClientes; i++)
         {
-            Console.WriteLine("Nota inválida. Informe novamente.");
-            nota = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe o tempo do atendimento:");
+            tempoAtendimento = Convert.ToInt32(Console.ReadLine());
+
+            tempoTotal = tempoTotal + tempoAtendimento;
         }
 
-        Console.WriteLine("Nota registrada: " + nota);
-    
+        tempoMedio = tempoTotal / quantidadeClientes;
+
+        Console.WriteLine("Tempo total de atendimento: " + tempoTotal + " minutos");
+        Console.WriteLine("Tempo médio por cliente: " + tempoMedio + " minutos");
+    }
