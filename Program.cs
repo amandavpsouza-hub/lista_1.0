e0cf5d9433200b937e7b9a304ea82eb8e2c689fd
Amanda <amanda_vp_souza@estudante.sesisenai.org.br>
2026-06-15 19:37:20 -0300

    q13

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
-﻿Console.WriteLine("Informe o número inicial da contagem regressiva:");
+﻿ Console.WriteLine("Informe o número da tabuada:");
 
         int numero = Convert.ToInt32(Console.ReadLine());
 
-        for (int i = numero; i >= 0; i--)
+        for (int i = 1; i <= 10; i++)
         {
-            Console.WriteLine(i);
+            Console.WriteLine(numero + " * " + i + " = " + (numero * i));
         }
-
-        Console.WriteLine("Fim da contagem.");
