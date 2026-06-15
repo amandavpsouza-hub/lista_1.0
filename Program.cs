807e288a97b0a36b998731ef6dfb62788c02ff32
Amanda <amanda_vp_souza@estudante.sesisenai.org.br>
2026-06-15 19:10:06 -0300

    q9

--- Program.cs
+++ Program.cs
@@ -1,26 +1,13 @@
-﻿
-        Console.WriteLine("Informe a primeira pontuação:");
-        int pontuacao1 = int.Parse(Console.ReadLine());
+﻿ 
+        Console.WriteLine("Informe um número inteiro:");
 
-        Console.WriteLine("Informe a segunda pontuação:");
-        int pontuacao2 = int.Parse(Console.ReadLine());
+        int numero = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Informe a terceira pontuação:");
-        int pontuacao3 = int.Parse(Console.ReadLine());
-
-        if (pontuacao1 == pontuacao2 && pontuacao2 == pontuacao3)
+        if (numero % 2 == 0)
         {
-            Console.WriteLine("As três pontuações são iguais.");
-        }
-        else if (pontuacao1 >= pontuacao2 && pontuacao1 >= pontuacao3)
-        {
-            Console.WriteLine("Maior pontuação: " + pontuacao1);
-        }
-        else if (pontuacao2 >= pontuacao1 && pontuacao2 >= pontuacao3)
-        {
-            Console.WriteLine("Maior pontuação: " + pontuacao2);
+            Console.WriteLine("O número informado é par.");
         }
         else
         {
-            Console.WriteLine("Maior pontuação: " + pontuacao3);
+            Console.WriteLine("O número informado é ímpar.");
         }
