963f79509babcce1fd01d136f46c464a47c205da
Amanda <amanda_vp_souza@estudante.sesisenai.org.br>
2026-06-15 19:32:16 -0300

    q11

--- Program.cs
+++ Program.cs
@@ -1,24 +1,10 @@
-﻿   Console.WriteLine("Informe a idade do passageiro:");
-        int idade = Convert.ToInt32(Console.ReadLine());
+﻿
+        Console.WriteLine("Informe o número final da contagem:");
 
-        Console.WriteLine("Informe o valor normal da passagem:");
-        double valorPassagem = Convert.ToDouble(Console.ReadLine());
+        int numeroFinal = Convert.ToInt32(Console.ReadLine());
 
-        double valorPagar;
-
-        if (idade <= 5)
+        for (int i = 1; i <= numeroFinal; i++)
         {
-            valorPagar = 0;
-        }
-        else if (idade >= 60)
-        {
-            valorPagar = valorPassagem / 2;
-        }
-        else
-        {
-            valorPagar = valorPassagem;
+            Console.WriteLine(i);
         }
 
-        Console.WriteLine("Valor normal da passagem: R$ " + valorPassagem);
-        Console.WriteLine("Valor a pagar: R$ " + valorPagar);
-    
