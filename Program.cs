1368306ab140283559f86eef0ee4d6820436ace6
Amanda <amanda_vp_souza@estudante.sesisenai.org.br>
2026-06-15 19:58:12 -0300

    q19

--- Program.cs
+++ Program.cs
@@ -1,21 +1,51 @@
-﻿    { int quantidadeClientes;
-        int tempoAtendimento;
-        int tempoTotal = 0;
-        int tempoMedio;
+﻿   int saldo;
+        int operacao;
+        int valor;
 
-        Console.WriteLine("Informe a quantidade de clientes atendidos:");
-        quantidadeClientes = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Informe o saldo inicial:");
+        saldo = Convert.ToInt32(Console.ReadLine());
 
-        for (int i = 1; i <= quantidadeClientes; i++)
+        operacao = 0;
+
+        while (operacao != 3)
         {
-            Console.WriteLine("Informe o tempo do atendimento:");
-            tempoAtendimento = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escolha a operação:");
+            Console.WriteLine("1 - Entrada de dinheiro");
+            Console.WriteLine("2 - Saída de dinheiro");
+            Console.WriteLine("3 - Encerrar");
 
-            tempoTotal = tempoTotal + tempoAtendimento;
+            operacao = Convert.ToInt32(Console.ReadLine());
+
+            if (operacao == 1)
+            {
+                Console.WriteLine("Informe o valor da movimentação:");
+                valor = Convert.ToInt32(Console.ReadLine());
+
+                saldo = saldo + valor;
+
+                Console.WriteLine("Entrada registrada. Saldo atual: R$ " + saldo);
+            }
+            else if (operacao == 2)
+            {
+                Console.WriteLine("Informe o valor da movimentação:");
+                valor = Convert.ToInt32(Console.ReadLine());
+
+                if (valor <= saldo)
+                {
+                    saldo = saldo - valor;
+                    Console.WriteLine("Saída registrada. Saldo atual: R$ " + saldo);
+                }
+                else
+                {
+                    Console.WriteLine("Saldo insuficiente.");
+                }
+            }
+            else if (operacao == 3)
+            {
+                Console.WriteLine("Saldo final: R$ " + saldo);
+            }
+            else
+            {
+                Console.WriteLine("Operação inválida.");
+            }
         }
-
-        tempoMedio = tempoTotal / quantidadeClientes;
-
-        Console.WriteLine("Tempo total de atendimento: " + tempoTotal + " minutos");
-        Console.WriteLine("Tempo médio por cliente: " + tempoMedio + " minutos");
-    }
